d5039943d04826cdf7a543e493668ff1fd08cd9a
Tech-Monica <monicapatel309@gmail.com>
2023-06-24 10:46:56 +0530

    first commit

---
+++ models/productModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema(
+    {
+        PET_ID: {
+            type: String,
+            required: true
+        },
+        PET_NAME: {
+            type: String,
+            required: [true, "Please enter a pet name"]
+            
+        },
+        PET_BREED: {
+            type: String,
+            required: true,
+        },
+        PET_TYPE: {
+            type: String,
+            required: true,
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
